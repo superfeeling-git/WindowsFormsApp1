e7d5f062964ba82ddbe8d3727d4895f79601dfc8
superfeeling-git <58456025+superfeeling-git@users.noreply.github.com>
2020-01-28 00:08:50 +0800

    初步完成Lucene

--- WebMVC/Controllers/DefaultController.cs
+++ WebMVC/Controllers/DefaultController.cs
@@ -14,6 +14,8 @@ using System.IO;
 using WebMVC.Models;
 using Lucene.Net.Analysis.Tokenattributes;
 using Lucene.Net.Analysis.PanGu;
+using Lucene.Net.Search;
+using PanGu.HighLight;
 
 namespace WebMVC.Controllers
 {
@@ -28,11 +30,18 @@ namespace WebMVC.Controllers
         [HttpPost]
         public ActionResult Index(News Model)
         {
-            foreach(var p in Analyzers(Model.Content,new PanGuAnalyzer()))
+            createIndex(Model);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Search(string Keywords)
+        {
+            if(Request.IsAjaxRequest())
             {
-                Response.Write(p + "<br>");
+                return Json(search(Keywords), JsonRequestBehavior.AllowGet);
             }
-            return null;
+            return View();
         }
 
         public List<string> Analyzers(string Content,Analyzer analyzer)
@@ -66,7 +75,7 @@ namespace WebMVC.Controllers
         private void createIndex(News Model)
         {
             //定义索引目录路径
-            string path = Server.MapPath("/Index");
+            string path = Server.MapPath("/Indexs");
 
             //定义一个分词器
             Analyzer analyzer = new PanGuAnalyzer();
@@ -92,7 +101,7 @@ namespace WebMVC.Controllers
 
                 doc.Add(new Field("Title", Model.Title, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_OFFSETS));
 
-                doc.Add(new Field("Content", Model.Content, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.WITH_OFFSETS));
+                doc.Add(new Field("Content", Model.Content, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_OFFSETS));
 
                 doc.Add(new Field("AddTime", Model.AddTime.ToString("yyyy-MM-dd HH:mm:ss"), Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_OFFSETS));
 
@@ -101,6 +110,68 @@ namespace WebMVC.Controllers
                 iw.Optimize();
             }
         }
+
+        public List<News> search(string keyWord)
+        {
+            //定义分词器
+            Analyzer analyzer = new PanGuAnalyzer();
+
+            //定义索引目录路径
+            string path = Server.MapPath("/Indexs");
+
+            //定义索引用到的目录
+            FSDirectory directory = FSDirectory.Open(new DirectoryInfo(path), new NoLockFactory());
+
+            //返回读取给定目录中索引的IndexReader。
+            //您应该传递readOnly =true，因为它提供了更好的并发性能，除非您打算对读取器执行写操作（删除文档或更改规范）。
+            IndexReader reader = IndexReader.Open(directory, true);
+
+            IndexSearcher searcher = new IndexSearcher(reader);
+
+            //设置查询
+            Query query = new TermQuery(new Term("Content", keyWord));
+
+            TopScoreDocCollector collector = TopScoreDocCollector.Create(1000, true);
+
+            // 使用query这个查询条件进行搜索，搜索结果放入collector
+            searcher.Search(query, null, collector);
+
+            // 从查询结果中取出第m条到第n条的数据
+            // collector.GetTotalHits()表示总的结果条数
+            ScoreDoc[] docs = collector.TopDocs(0, collector.TotalHits).ScoreDocs;
+
+            // 遍历查询结果
+            List<News> resultList = new List<News>();
+            for (int i = 0; i < docs.Length; i++)
+            {
+                // 拿到文档的id，因为Document可能非常占内存（DataSet和DataReader的区别）
+                int docId = docs[i].Doc;
+                //// 所以查询结果中只有id，具体内容需要二次查询
+                //// 根据id查询内容：放进去的是Document，查出来的还是Document
+                Document doc = searcher.Doc(docId);
+                News result = new News();
+                result.Id = Convert.ToInt32(doc.Get("id"));
+
+                result.Title = doc.Get("Title");
+
+                result.AddTime = Convert.ToDateTime(doc.Get("AddTime"));
+
+                SimpleHTMLFormatter formatter = new SimpleHTMLFormatter("<font color='red'>", "</font>");
+                //构造一个高亮对象，它将应用改革才创建的格式化
+                Highlighter highter = new Highlighter(formatter, new PanGu.Segment());
+
+                //设置片段的长度，应该是格式化搜索词后带html标签的长度 
+                highter.FragmentSize = 120;
+
+                //调用方法，替换数据title中的关键词，也就是高亮此关键词                
+                result.Content = highter.GetBestFragment(keyWord, doc.Get("Content"));
+
+                resultList.Add(result);
+            }
+
+            return resultList;
+        }
+
 
     }
 }
